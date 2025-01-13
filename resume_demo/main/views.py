5f5a41ed26786496594691cbdc1550e116970af1
MoraStok <morastok@gmail.com>
2025-01-13 14:31:04 +0100

    first commit

---
+++ resume_demo/main/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.contrib import messages
+from .models import (
+		UserProfile,
+		Certificate
+	)
+from django.views import generic
+from . forms import ContactForm
+
+class IndexView(generic.TemplateView):
+	template_name = "main/index.html"
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		
+		certificates = Certificate.objects.filter(is_active=True)
+		
+		context["certificates"] = certificates
+		return context
+
+
+class ContactView(generic.FormView):
+	template_name = "main/contact.html"
+	form_class = ContactForm
+	success_url = "/"
+	
+	def form_valid(self, form):
+		form.save()
+		messages.success(self.request, 'Thank you. We will be in touch soon.')
+		return super().form_valid(form)
