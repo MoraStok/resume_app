790f52e4ec0a67f3195acbe7a701904ac9414f70
MoraStok <morastok@gmail.com>
2025-01-19 12:15:09 +0100

    Changes to ALLOWED_HOSTS

--- resume_demo/resume_demo/settings.py
+++ resume_demo/resume_demo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5^@=ufpzp=4gwt^#$wn-djs+e5*a*8n8q0eu#6km)cmu9nxe%#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]", "moritaiam.com", "www.moritaiam.com"]
 
 
 # Application definition
